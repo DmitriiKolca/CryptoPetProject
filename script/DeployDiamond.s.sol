2ad103eeb0d386d4b99c391c3c0674a294ce2698
DmitriiKolca <leonidi4@list.ru>
2026-09-15 12:46:14 +0300

    Add deploy script and tests for admin facet

---
+++ script/DeployDiamond.s.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Script} from "forge-std/Script.sol";
+import {Diamond} from "../lib/diamond-3-hardhat/contracts/Diamond.sol";
+import {DiamondCutFacet} from "../lib/diamond-3-hardhat/contracts/facets/DiamondCutFacet.sol";
+import {AdminFacet} from "../src/AdminFacet.sol";
+import {MintFacet} from "../src/MintFacet.sol";
+import {InitFacet} from "../src/InitFacet.sol";
+import {IDiamondCut} from "../lib/diamond-3-hardhat/contracts/interfaces/IDiamondCut.sol";
+
+contract DeployDiamondScript is Script {
+    struct DeploymentResult {
+        address diamondAddress;
+        AdminFacet adminFacet;
+        MintFacet mintFacet;
+    }
+
+    function run(address owner) external returns (DeploymentResult memory) {
+        vm.startBroadcast(owner);
+
+        DiamondCutFacet diamondCutFacet = new DiamondCutFacet();
+        Diamond diamond = new Diamond(owner, address(diamondCutFacet));
+
+        AdminFacet adminFacet = new AdminFacet();
+        InitFacet initFacet = new InitFacet();
+        MintFacet mintFacet = new MintFacet();
+
+        bytes4[] memory adminSelectors = getAdminSelectors();
+        bytes4[] memory mintSelectors = getMintSelectors();
+
+        IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](2);
+        cut[0] = IDiamondCut.FacetCut({
+            facetAddress: address(adminFacet),
+            action: IDiamondCut.FacetCutAction.Add,
+            functionSelectors: adminSelectors
+        });
+        cut[1] = IDiamondCut.FacetCut({
+            facetAddress: address(mintFacet),
+            action: IDiamondCut.FacetCutAction.Add,
+            functionSelectors: mintSelectors
+        });
+
+       bytes memory initCallData = abi.encodeWithSignature(
+            "init(string,string,string)",
+            "My Crypto Game",
+            "MCG",
+            "https://avatars.mds.yandex.net/i?id=2730c6b9a82576cad8ea336decd83f47945a5c53-12373036-images-thumbs&n=13"
+        );
+
+        IDiamondCut(address(diamond)).diamondCut(cut, address(initFacet), initCallData);
+
+        vm.stopBroadcast();
+
+        return DeploymentResult({
+            diamondAddress: address(diamond),
+            adminFacet: adminFacet,
+            mintFacet: mintFacet
+        });
+    }
+
+    function getAdminSelectors() internal pure returns (bytes4[] memory) {
+        bytes4[] memory selectors = new bytes4[](5);
+        selectors[0] = AdminFacet.addAdmin.selector;
+        selectors[1] = AdminFacet.isAdmin.selector;
+        selectors[2] = AdminFacet.removeAdmin.selector;
+        selectors[3] = AdminFacet.setBaseURI.selector;
+        selectors[4] = AdminFacet.setCustomNftURIByAdmin.selector;
+        return selectors;
+    }
+
+    function getMintSelectors() internal pure returns (bytes4[] memory) {
+        bytes4[] memory selectors = new bytes4[](11);
+        selectors[0] = MintFacet.mint.selector;
+        selectors[1] = bytes4(keccak256("balanceOf(address)"));
+        selectors[2] = bytes4(keccak256("ownerOf(uint256)"));
+        selectors[3] = bytes4(keccak256("safeTransferFrom(address,address,uint256)"));
+        selectors[4] = bytes4(keccak256("safeTransferFrom(address,address,uint256,bytes)"));
+        selectors[5] = bytes4(keccak256("transferFrom(address,address,uint256)"));
+        selectors[6] = bytes4(keccak256("approve(address,uint256)"));
+        selectors[7] = bytes4(keccak256("getApproved(uint256)"));
+        selectors[8] = bytes4(keccak256("setApprovalForAll(address,bool)"));
+        selectors[9] = bytes4(keccak256("isApprovedForAll(address,address)"));
+        selectors[10] = bytes4(keccak256("tokenURI(uint256)"));
+        return selectors;
+    }
+}
---
+++ test/AdminFacet.t.sol
@@ -0,0 +1,235 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+import {AdminFacet} from "../src/AdminFacet.sol";
+import {MintFacet} from "../src/MintFacet.sol";
+import {IDiamondCut} from "../lib/diamond-3-hardhat/contracts/interfaces/IDiamondCut.sol";
+import {Diamond} from "../lib/diamond-3-hardhat/contracts/Diamond.sol";
+import {DiamondCutFacet} from "../lib/diamond-3-hardhat/contracts/facets/DiamondCutFacet.sol";
+import {ERC721MetadataStorage} from "@solidstate-network/contracts/token/ERC721/metadata/ERC721MetadataStorage.sol";
+
+import {DeployDiamondScript} from "../script/DeployDiamond.s.sol";
+
+import {console} from "../lib/forge-std/src/console.sol";
+
+contract AdminFacetTest is Test {
+    Diamond public diamond;
+    DiamondCutFacet public diamondCutFacetInstance;
+    AdminFacet public adminFacetAsDiamond; // Прокси, обернутый в ABI фасета
+    MintFacet public mintFacetAsDiamond;
+    AdminFacet public adminFacetInstance;  // Сам развернутый контракт фасета
+    MintFacet public mintFacetInstance;
+
+    address public owner = address(1);
+    address public alien = address(2);
+    address public newAdmin = address(3);
+    address public nftReceiver  = address(4);
+
+    function setUp() public {
+        DeployDiamondScript deployer = new DeployDiamondScript();
+        DeployDiamondScript.DeploymentResult memory result = deployer.run(owner);
+
+        adminFacetAsDiamond = AdminFacet(result.diamondAddress);
+        mintFacetAsDiamond = MintFacet(result.diamondAddress);
+    }
+
+    /*  Tests for addAdmin function */
+    function test_addAdmin_ShouldRevert_WhenNotOwner() public {
+        vm.startPrank(alien);
+
+        vm.expectRevert("LibDiamond: Must be contract owner");
+        adminFacetAsDiamond.addAdmin(newAdmin);
+
+        vm.stopPrank();
+    }
+    function test_addAdmin_ShouldRevert_WhenZeroAddress() public {
+        vm.startPrank(owner);
+
+        vm.expectRevert("Admin cannot be zero address");
+        adminFacetAsDiamond.addAdmin(address(0));
+
+        vm.stopPrank();
+    }
+    function test_addAdmin_ShouldRevert_WhenAlreadyAdmin() public {
+        vm.startPrank(owner);
+        adminFacetAsDiamond.addAdmin(newAdmin);
+
+        vm.expectRevert("Address is already admin");
+        adminFacetAsDiamond.addAdmin(newAdmin);
+
+        vm.stopPrank();
+    }
+    function test_addAdmin_Success() public {
+        vm.startPrank(owner);
+
+        vm.expectEmit(true, false, false, false);
+        emit AdminFacet.AdminAdded(newAdmin);
+
+        adminFacetAsDiamond.addAdmin(newAdmin);
+        vm.stopPrank();
+
+        assertTrue(adminFacetAsDiamond.isAdmin(newAdmin));
+    }
+
+    /*  Tests for removeAdmin function */
+    function test_removeAdmin_ShouldRevert_WhenNotOwner() public {
+        vm.startPrank(alien);
+
+        vm.expectRevert("LibDiamond: Must be contract owner");
+        adminFacetAsDiamond.removeAdmin(newAdmin);
+
+        vm.stopPrank();
+    }
+    function test_removeAdmin_ShouldRevert_WhenZeroAddress() public {
+        vm.startPrank(owner);
+
+        vm.expectRevert("Admin cannot be zero address");
+        adminFacetAsDiamond.removeAdmin(address(0));
+
+        vm.stopPrank();
+    }
+    function test_removeAdmin_ShouldRevert_WhenNotAdmin() public {
+        vm.startPrank(owner);
+
+        vm.expectRevert("Address is not an admin");
+        adminFacetAsDiamond.removeAdmin(newAdmin);
+
+        vm.stopPrank();
+    }
+    function test_removeAdmin_ShouldRevert_WhenAlreadyRemoved() public {
+        vm.startPrank(owner);
+        adminFacetAsDiamond.addAdmin(newAdmin);
+        adminFacetAsDiamond.removeAdmin(newAdmin);
+
+        vm.expectRevert("Address is not an admin");
+        adminFacetAsDiamond.removeAdmin(newAdmin);
+
+        vm.stopPrank();
+    }
+    function test_removeAdmin_Success() public {
+        vm.startPrank(owner);
+        adminFacetAsDiamond.addAdmin(newAdmin);
+
+        vm.expectEmit(true, false, false, false);
+        emit AdminFacet.AdminRemoved(newAdmin);
+
+        adminFacetAsDiamond.removeAdmin(newAdmin);
+        vm.stopPrank();
+
+        assertFalse(adminFacetAsDiamond.isAdmin(newAdmin));
+    }
+
+    /*  Tests for isAdmin function */
+    function test_isAdmin_ShouldNorRevert_WhenCallerNotOwner() public {
+        vm.prank(alien);
+
+        try adminFacetAsDiamond.isAdmin(newAdmin) returns (bool result) {
+            assertFalse(result);
+        } catch {
+            fail("The call reverted, but it should NOT have reverted");
+        }
+    }
+    function test_isAdmin_ShouldNorRevert_WhenCallerNotAdmin() public {
+        vm.prank(alien);
+
+        try adminFacetAsDiamond.isAdmin(newAdmin) returns (bool result) {
+            assertFalse(result);
+        } catch {
+            fail("The call reverted, but it should NOT have reverted");
+        }
+    }
+    function test_isAdmin_ShouldNorRevert_WhenCallerAdmin() public {
+        vm.prank(owner);
+        adminFacetAsDiamond.addAdmin(newAdmin);
+        vm.stopPrank();
+
+        vm.prank(newAdmin);
+
+        try adminFacetAsDiamond.isAdmin(newAdmin) returns (bool result) {
+            assertTrue(result);
+        } catch {
+            fail("The call reverted, but it should NOT have reverted");
+        }
+    }
+    function test_isAdmin_ShouldRevertTrue_WhenUserIsAdmin() public {
+        vm.prank(owner);
+        adminFacetAsDiamond.addAdmin(newAdmin);
+
+        assertTrue(adminFacetAsDiamond.isAdmin(newAdmin));
+
+        vm.stopPrank();
+    }
+    function test_isAdmin_ShouldRevertFalse_WhenUserNotAdmin() public {
+        vm.startPrank(alien);
+
+        assertFalse(adminFacetAsDiamond.isAdmin(newAdmin));
+
+        vm.stopPrank();
+    }
+
+    /*  Tests for setBaseURI function */
+    function test_setBaseURI_ShouldRevert_WhenNotOwner() public {
+        vm.startPrank(alien);
+
+        vm.expectRevert("LibDiamond: Must be contract owner");
+        adminFacetAsDiamond.setBaseURI("https://mygame.com");
+
+        vm.stopPrank();
+    }
+    function test_setBaseURI_ShouldReturnBaseUri() public {
+        uint256 nftId = 1;
+        mintFacetAsDiamond.mint();
+        string memory initUriFromScript = "https://avatars.mds.yandex.net/i?id=2730c6b9a82576cad8ea336decd83f47945a5c53-12373036-images-thumbs&n=13";
+        string memory nftIdPostfix = "_1";
+        string memory resultNftUri = string.concat(initUriFromScript, nftIdPostfix);
+
+        string memory baseTokenUri = mintFacetAsDiamond.tokenURI(nftId);
+        console.log("base_init_uri", resultNftUri);
+
+        assertEq(baseTokenUri, resultNftUri);
+    }
+    function test_setBaseURI_ShouldReturnUpdateBaseUri() public {
+        uint256 nftId = 1;
+        mintFacetAsDiamond.mint();
+
+        vm.prank(owner);
+        adminFacetAsDiamond.setBaseURI("https://mygame.com");
+
+        string memory actualTokenUri = mintFacetAsDiamond.tokenURI(nftId);
+        console.log("actual_URI", actualTokenUri);
+        assertEq(actualTokenUri, "https://mygame.com_1");
+    }
+
+    /*  Tests for setCustomNftURIByAdmin function */
+    function test_setCustomNftURIByAdmin_ShouldRevert_WhenNftNotExist() public {
+        vm.startPrank(alien);
+
+        vm.expectRevert("ERC721: token does not exist");
+        adminFacetAsDiamond.setCustomNftURIByAdmin(1,"https://mygame.com");
+
+        vm.stopPrank();
+    }
+    function test_setCustomNftURIByAdmin_ShouldRevert_WhenNotAdmin() public {
+        vm.startPrank(alien);
+        uint256 nftId = 1;
+        mintFacetAsDiamond.mint();
+
+        vm.expectRevert("Caller is not Admin");
+        adminFacetAsDiamond.setCustomNftURIByAdmin(1,"https://custom_uri.com");
+
+        vm.stopPrank();
+    }
+    function test_setCustomNftURIByAdmin_ShouldSuccess() public {
+        uint256 nftId = 1;
+        mintFacetAsDiamond.mint();
+        string memory customUri = "https://custom_uri.com";
+
+        vm.prank(owner);
+        adminFacetAsDiamond.setCustomNftURIByAdmin(nftId,customUri);
+
+        string memory actualTokenUri = mintFacetAsDiamond.tokenURI(nftId);
+        console.log("actual_URI", actualTokenUri);
+        assertEq(actualTokenUri, customUri);
+    }
+}
