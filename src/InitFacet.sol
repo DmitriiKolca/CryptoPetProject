55468d14fbf33d26b3acfcfdf22434c9136e6d9d
DmitriiKolca <leonidi4@list.ru>
2026-09-15 13:56:29 +0300

    Add support interfaces

--- src/InitFacet.sol
+++ src/InitFacet.sol
@@ -5,19 +5,25 @@ import "../lib/diamond-3-hardhat/contracts/libraries/LibDiamond.sol";
 import "../lib/solidstate-solidity/contracts/token/ERC721/SolidStateERC721.sol";
 import "./StorageFacet.sol";
 import {ERC721MetadataStorage} from "@solidstate-network/contracts/token/ERC721/metadata/ERC721MetadataStorage.sol";
+import {ERC165BaseStorage} from "@solidstate-network/contracts/introspection/ERC165/base/ERC165BaseStorage.sol";
 
-contract InitFacet is SolidStateERC721, StorageFacet {
-    function init(string calldata _name, string calldata _symbol, string calldata _baseUri) external {
+contract InitFacet is StorageFacet {
+    function init(string memory _name, string memory _symbol, string memory _baseUri) external {
         LibDiamond.enforceIsContractOwner();
         address contractOwner = LibDiamond.contractOwner();
 
         Admins storage adminStore = getAdmins();
-
         adminStore.adminList[contractOwner] = true;
 
         ERC721MetadataStorage.Layout storage metadataLayout = ERC721MetadataStorage.metadataLayout();
         metadataLayout.name = _name;
         metadataLayout.symbol = _symbol;
         metadataLayout.baseURI = _baseUri;
+
+        ERC165BaseStorage.Layout storage erc165Layout = ERC165BaseStorage.metadataLayout();
+        erc165Layout.supportedInterfaces[0x01ffc9a7] = true; // ERC165 interface ID
+        erc165Layout.supportedInterfaces[0x80ac58cd] = true; // ERC721 interface ID
+        erc165Layout.supportedInterfaces[0x5b5e139f] = true; // ERC721Metadata interface ID
+
     }
 }
